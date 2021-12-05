11461336dbd0f024d997569df4ae7f3c9cd002a3
tyssmit <tysonsmith452@gmail.com>
2021-12-05 16:07:04 -0600

    creating socket connection via python

--- SocketConnection/SocketConnection.py
+++ SocketConnection/SocketConnection.py
@@ -1,1 +1,12 @@
+import socket
 
+def main():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host='localhost'
+    port=5000
+    sock.bind((host,port))
+    sock.connect((host,port))
+    print('Congrats we are connected')
+    sock.close
+if __name__ == "__main__":
+    main()
